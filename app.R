d9f1aec78f862f758e59aeeae2f635c06d2e08b9
John Coene <jcoenep@gmail.com>
2018-01-01 23:10:13 +0100

    demo

--- app.R
+++ app.R
@@ -2,92 +2,100 @@ library(fullPage)
 library(shiny)
 
 options <- list(
-  sectionsColor = c("#4BBFC3", "#7BAABE", "lightgray")
+  sectionsColor = c("#E0FFFA", "#CEFFF2", "#C0F9F0", "#90F9EA", "#394C54", "#E0FFFA")
 )
 
 ui <- multiPage(
   opts = options,
-  menu = c("Multi Scroll" = "multiscroll",
-           "Plots" = "plots",
-           "Full Section plots" = "fullplots",
-           "More" = "more"),
+  multiTheme('aqua'),
+  menu = c(
+    "multiPage" = "multipage",
+    "Split" = "split",
+    "Grid" = "grid",
+    "Plot" = "plot",
+    "Buttons" = "buttons",
+    "Sisters" = "sisters"
+  ),
   multiLeft(
+    align = "right",
     multiSection(
-      center = TRUE,
-      menu = "multiscroll",
-      h1("Multiple")
+      menu = "multipage",
+      h1("multiScroll.js")
     ),
     multiSection(
-      menu = "plots",
+      menu = "split",
+      center = TRUE,
+      h1("Split")
+    ),
+    multiSection(
+      center = TRUE,
+      menu = "grid",
+      multiRow(
+        multiColumn("First Column"),
+        multiColumn("Second Column")
+      )
+    ),
+    multiSection(
+      menu = "plot",
+      center = TRUE,
       multiContainer(
-        h1("Plot ->"),
-        numericInput(
-          "max",
-          "Maximum",
-          min = 50,
-          max = 250,
-          value = 10
+        h1("Full Section Plot"),
+        selectInput(
+          "obs",
+          "Select",
+          c(10, 20 ,50 ,100 , 150)
         )
       )
     ),
-    multiSectionPlot(
-      menu = "fullplots",
-      "fullPlot"
+    multiSection(
+      menu = "buttons",
+      center = TRUE,
+      h1("Add buttons", style = "color: white;")
     ),
     multiSection(
-      menu = "more",
-      center = TRUE,
-      multiContainer(
-        p("Want more Stuff like this?"),
-        p("->")
-      )
+      menu = "sisters",
+      h1("More like this ->")
     )
   ),
   multiRight(
+    align = "left",
+    multiSection(
+      h1("-Shiny")
+    ),
+    multiSection(
+      plotOutput("plot1")
+    ),
+    multiSection(
+      h1("<- Grid")
+    ),
+    multiSectionPlot("plot2"),
     multiSection(
       center = TRUE,
-      h1("scroll")
+      multiButtonDown("Next section", outline = TRUE)
     ),
     multiSection(
       center = TRUE,
-      plotOutput("plot")
-    ),
-    multiSection(
-      multiContainer(
-        center = TRUE,
-        h1("<- Full Section plot"),
-        sliderInput(
-          "number",
-          "Data points",
-          min = 50,
-          max = 250,
-          value = 10
-        )
-      )
-    ),
-    multiSection(
-      multiButton(
-        href = "http://john-coene.com",
-        "Click me!"
-      ),
-      p(
-        "This run just fine... the issue is with showcase mode."
-      )
+      verbatimTextOutput("sistersDemo")
     )
   )
 )
 
 server <- function(input, output){
-  
-  output$plot <- renderPlot({
-    hist(rnorm(100, 20, input$max))
+  output$plot1 <- renderPlot({
+    par(bg = "#CEFFF2")
+    plot(mtcars$mpg, mtcars$wt)
   })
   
-  output$fullPlot <- renderPlot({
-    par(bg = "gray")
-    hist(rnorm(input$number, 20, 250))
+  output$plot2 <- renderPlot({
+    par(bg = "#C0F9F0")
+    hist(rnorm(input$obs, 10, 5))
+  })
+  
+  output$sistersDemo <- renderText({
+    '# See those
+    demo("fullPage", package = "fullPage")
+    demo("multiPage", package = "fullPage")'
   })
 }
 
 shinyApp(ui, server)
-
